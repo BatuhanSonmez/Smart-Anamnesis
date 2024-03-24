87f19dea6d8d6182a77ccaf349d3eb113c864510
Batuhan Sönmez <Batuhan.Sonmez@live.acibadem.edu.tr>
2024-03-24 14:22:58 +0300

    Project to-do list created, main.py created
    Project to-do list created, main.py created

---
+++ module1/module1.py
@@ -0,0 +1,4 @@
+#TO-DO:
+#1- Ses dosyalarının aynı formata ve sr (Speech Recognition Library) tarafından desteklenen bir hale getirilmesi.
+#2- Aynı formattaki ses dosyalarının transcription'ı. - Recognize_Google kullanarak.
+#3- Kullanılacak olan modulün iyice araştırılması.
---
+++ module2/module2.py
@@ -0,0 +1,4 @@
+#TO-DO:
+#1- NLP (Natural Language Processing) nasıl yapılır? Bunun araştırması yapılacak.
+#2- NER (Named Entity Recognition) - Labeling nasıl yapaılır? Araştırılacak. 
+#3- Hangi Python kütüphanesi NER - Labeling için kullanılacak? -> Doccano
